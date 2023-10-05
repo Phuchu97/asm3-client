ae223dbf0252ca9e255d6dd0c5c5b2b50094583b
Phuchu97 <103049585+Phuchu97@users.noreply.github.com>
2023-10-05 21:44:52 +0700

    Update Categories.jsx

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -55,8 +55,8 @@ function CategoriesComponent() {
             <div className="down-animation"><i class="fa-solid fa-angle-down"></i></div>
         </div>
         <div className="categories-title" data-aos="fade-up" data-aos-duration="1000">
-            <h2>CÔNG TY TNHH THÉP VƯỢNG PHÁT</h2>
-            <p>HỢP TÁC CÙNG PHÁT TRIỂN</p>
+            <h2>SẢN PHẨM VƯỢT TRỘI VỀ CHẤT LƯỢNG</h2>
+            <p>CHÚNG TÔI THẾ MẠNH TRONG NHIỀU LĨNH VỰC</p>
         </div>
         <div className="category row" data-aos="fade-up" data-aos-duration="2000">
             {
