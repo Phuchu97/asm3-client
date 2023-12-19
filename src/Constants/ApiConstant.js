25ba602d6212915551bcd2f8230ec0b53ca00375
Phuchu97 <phuchu199749@gmail.com>
2023-12-19 11:08:57 +0700

    Update ApiConstant.js

--- src/Constants/ApiConstant.js
+++ src/Constants/ApiConstant.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:4000';
-// export const API_URL = 'https://sale-api-mqqc.onrender.com';
+// export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://sale-api-mqqc.onrender.com';
