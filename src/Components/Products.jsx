5d003861e1b41e866314394f2d418e1610f5d459
vanphuchu <van.phu.chu@draphony.com>
2024-01-01 11:24:08 +0700

    update 1/1

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -5,13 +5,59 @@ import { API_URL } from "../Constants/ApiConstant";
 import { ColorRing } from 'react-loader-spinner';
 import '../css/products.css'
 import Numeral from 'react-numeral';
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import Slider from 'react-slick';
 
 
 function ProductsComponent() {
 
     const [isLoading, setIsLoading] = useState(true);
     const [listProducts, setListProducts] = useState([]);
+    var settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1480,
+                settings: {
+                    slidesToShow: 5,
+                },
+            },
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4,
+                },
+            },
+            {
+                breakpoint: 1039,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    autoplay: true,
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 549,
+                settings: {
+                    autoplay: true,
+                    slidesToShow: 1,
+                    dots: true,
+                },
+            },
+        ],
+    };
 
     useEffect(() => {
         getListProducts((res) => {
@@ -33,44 +79,47 @@ function ProductsComponent() {
                     <p>SẢN PHẨM HÀNG ĐẦU</p>
                 </div>
             </div>
-                {
-                    isLoading ? <div className={isLoading ? 'active' : 'not-active'}>
-                        <Grid display={'flex'} justifyContent={'center'}>
-                            <ColorRing
-                                visible={true}
-                                height="80"
-                                width="80"
-                                ariaLabel="blocks-loading"
-                                wrapperStyle={{}}
-                                wrapperClass="blocks-wrapper"
-                                colors={['#f0d29c', '#c5a568', '#ccb286', '#d2b789', '#afa999']}
-                            />
-                        </Grid>
-                    </div> : <div className="products">
-                        <div className="product row" data-aos="fade-up" data-aos-duration="1000">
-                            {
-                                listProducts.length > 0 && listProducts.map(obj => {
-                                    return (
-                                        <div className="col-xs-12 col-sm-12 col-md-3 col-lg-3 mt-4">
-                                            <Link to={`/product-detail/${obj._id}`} style={{ textDecoration: 'none' }}>
-                                                <div className="product-item">
-                                                    <img src={obj.image[0]} alt="product" />
-                                                </div>
-                                                <div className="product-content">
-                                                    <h4>{obj.name}</h4>
-                                                    <p><Numeral value={obj.price} format={"0,0"} /> VND</p>
-                                                </div>
-                                            </Link>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
+            {
+                isLoading ? <div className={isLoading ? 'active' : 'not-active'}>
+                    <Grid display={'flex'} justifyContent={'center'}>
+                        <ColorRing
+                            visible={true}
+                            height="80"
+                            width="80"
+                            ariaLabel="blocks-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="blocks-wrapper"
+                            colors={['#f0d29c', '#c5a568', '#ccb286', '#d2b789', '#afa999']}
+                        />
+                    </Grid>
+                </div> : 
+                <div className="products">
+                    <div className="product " data-aos="fade-up" data-aos-duration="1000">
+                    <Slider {...settings}>
+                        {
+                            listProducts.length > 0 && listProducts.map(obj => {
+                                return (
+                                    <Grid className="product-slide-item">
+                                        <Link to={`/product-detail/${obj._id}`} style={{ textDecoration: 'none' }}>
+                                            <Grid className="product-item">
+                                                <img src={obj.image[0]} alt="product" />
+                                            </Grid>
+                                            <Grid className="product-content">
+                                                <Typography variant="h4">{obj.name}</Typography>
+                                                <Typography><Numeral value={obj.price} format={"0,0"} /> VND</Typography>
+                                            </Grid>
+                                        </Link>
+                                    </Grid>
+                                )
+                            })
+                        }
+                        </Slider>
                     </div>
-                }
-            </div>
+                </div>
+            }
+        </div>
 
-            );
+    );
 }
 
-            export default ProductsComponent;
+export default ProductsComponent;
