ca6b6acc0283cb932e5d2d2a30d8aa1b5873518d
phuchu1997 <phucv@amcolab.vn>
2024-06-19 23:06:29 +0700

    Update ApiConstant.js

--- src/Constants/ApiConstant.js
+++ src/Constants/ApiConstant.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:4000';
-// export const API_URL = 'https://asm-api-xi.vercel.app';
+// export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://asm-api-xi.vercel.app';
