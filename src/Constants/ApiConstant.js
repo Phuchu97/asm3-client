2d5288782e396b83f3094184d50d1de81b7a9b0f
phuchu1997 <phucv@amcolab.vn>
2024-06-30 19:45:57 +0700

    Update ApiConstant.js

--- src/Constants/ApiConstant.js
+++ src/Constants/ApiConstant.js
@@ -1,2 +1,3 @@
 // export const API_URL = 'http://localhost:4000';
-export const API_URL = 'https://sale-api-mqqc.onrender.com';
+// export const API_URL = 'https://sale-api-mqqc.onrender.com';
+export const API_URL = 'http://123.31.39.187:4000';
