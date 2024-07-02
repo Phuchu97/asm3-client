4ab7530bcf8463be16d12d0920e03bd94b945995
phuchu1997 <phucv@amcolab.vn>
2024-07-02 23:19:44 +0700

    Update ApiConstant.js

--- src/Constants/ApiConstant.js
+++ src/Constants/ApiConstant.js
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://localhost:4000';
 // export const API_URL = 'https://sale-api-mqqc.onrender.com';
-export const API_URL = 'http://103.152.165.223:4000';
+export const API_URL = 'https://vuongphatsteel.com/api';
